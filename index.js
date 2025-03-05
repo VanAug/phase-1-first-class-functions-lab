a0a2c67c96aa1b3ae7601aec2432bdea67ba0dfa
Brian <okellobrian18@gmail.com>
2025-03-05 13:45:33 +0300

    More on functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = drivers => drivers.slice(0,2)
+
+const returnLastTwoDrivers = drivers => drivers.slice(2)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    function multiply(fare) {
+        return fare * multiplier
+    }
+    return multiply
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, returnLastTwoDrivers) => {
+    return returnLastTwoDrivers(drivers)
+}
+
+console.log(createFareMultiplier(2)(40))
+
+console.log(fareDoubler(40))
+
+console.log(fareTripler(40))
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers))
